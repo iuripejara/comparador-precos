529809ba961f83820365f39099576c42ace6b797
iuripejara <167295209+iuripejara@users.noreply.github.com>
2026-08-16 22:17:57 +0100

    continente
    pesquisa de compras 16-08-26

--- backend/supermercados/continente.py
+++ backend/supermercados/continente.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 import time
 
 
@@ -7,10 +8,15 @@ import time
 navegar = webdriver.Chrome()
 navegar.get("https://www.continente.pt/")
 
+
+
+
+cokisEnter = navegar.find_element(By.XPATH,"//*[@id='CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll']")
+time.sleep(5)
+cokisEnter.send_keys(Keys.ENTER)
+
+searchElente = navegar.find_element(By.CLASS_NAME, "form-control.search-field.pwc-form-input.pwc-search-input")
+searchElente.send_keys("arroz")
+searchElente.send_keys(Keys.ENTER)
 time.sleep(3)
-title = navegar.title
-
-
-textNAmeCategory = navegar.find_element("class name", "col-12.textbox-container").text
-print(textNAmeCategory)
 navegar.quit() 
